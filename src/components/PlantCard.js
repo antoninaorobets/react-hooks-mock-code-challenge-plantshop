25eca3d7b532bdaf0e91877967db60be736a6bd0
antoninaorobets <pani.antonina.don@gmail.com>
2022-02-04 12:40:15 -0500

    lab

--- src/components/PlantCard.js
+++ src/components/PlantCard.js
@@ -2,11 +2,27 @@ import React,{useState} from "react";
 
 function PlantCard({plant}) {
   const [isInStock, setIsInStock]= useState(true)
+  const [price,szetPrice]=useState(plant.price)
+
+  const handlePriceChange = (event) => {
+    fetch(`http://localhost:6001/plants/${plant.id}`,{
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        ...plant,
+        price: [event.target.value]
+      })
+    }).then(resp=>resp.json()).then(data=>console.log(data))
+    szetPrice(event.target.value)
+  }
+
   return (
     <li className="card">
       <img src={plant.image} alt={plant.name} />
       <h4>{plant.name}</h4>
-      <p>Price: {plant.price}</p>
+      <p>Price: <input type="number" name="price" value={price} onChange={handlePriceChange}></input> </p>
       {isInStock ? (
         <button className="primary" onClick={()=>setIsInStock(!isInStock)}>In Stock</button>
       ) : (
